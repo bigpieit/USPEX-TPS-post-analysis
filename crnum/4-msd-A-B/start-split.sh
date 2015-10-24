96ca3d6a8c247e30c0542b23dd055d8def7d03e5
Bingxi Li <bigpieit@users.noreply.github.com>
2015-10-24 11:08:18 -0700

    Create start-split.sh

---
+++ crnum/4-msd-A-B/start-split.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+file=$(ls|grep crnum_all.crn)
+let atom=$(sed -n "4p" $file)
+let lines=$(nl $file|wc -l|cut -c 2-)
+let stpdif=$(sed -n "$[$atom+9+2]"p $file)
+./split-2.py $file $atom $lines $stpdif
